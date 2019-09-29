bac0cb1ad022707cc81ef80cae12168250840389
Sara R <sarararismiralles@gmail.com>
2019-09-29 19:40:32 +0200

    Non-clustering loss model

--- clustering.py
+++ clustering.py
@@ -40,17 +40,20 @@ import cv2, numpy as np
 
 
 def parse_args():
+    
     """Parse input arguments."""
     parser = argparse.ArgumentParser(description='Unsupervised learning using CNNs.')
     parser.add_argument('--model',default='ResNet50', type=str)
     parser.add_argument('--clustering_method',default='Kmeans', type=str)
+    parser.add_argument('--loss',default=None, type=str)
     args = parser.parse_args()
+    
     return args
 
 
 
 def data_preparation(train_path, test_path):
-
+    
     #Reading the training data and training labels
     x_train = []
     y_train = []
@@ -84,91 +87,107 @@ def data_preparation(train_path, test_path):
     #Normalize inputs from [0, 255] to [0, 1]
     x_train = x_train / 255
     x_test = x_test / 255
-    
-    #Convert class vectors to binary class matrices
-    n_classes = len(unique_labels(y_train))
-    y_train = np_utils.to_categorical(np.array(y_train)-1, num_classes = n_classes)
 
     return x_train, y_train, x_test, y_test
 
 
 
-def train_model(x_train, y_train, batch, x_test, y_test, model_name):
+def train_model(x_train, y_train, batch, x_test, y_test, model_name, loss):
     
     #Define model and other parameters
     if model_name == 'ResNet50':
-        model = InceptionResNetV2(include_top=False, weights='imagenet', input_shape = (166, 166, 3), pooling = 'avg')
-        num_epochs = 15
-    elif model_name == 'Xception':
-        model = InceptionResNetV2(include_top=False, weights='imagenet', input_shape = (166, 166, 3), pooling = 'avg')
-        num_epochs = 25
+        if loss == 'clustering':
+            model = load_model(model_name, 'None')
+            model = Model(inputs = model.input, outputs=model.layers[-2].output)
+            num_epochs = 35
+        else:
+            model = ResNet50(include_top=False, weights='imagenet', input_shape = (166, 166, 3), pooling = 'avg')
+            num_epochs = 15
     elif model_name == 'InceptionResNetV2':
         model = InceptionResNetV2(include_top=False, weights='imagenet', input_shape = (166, 166, 3), pooling = 'avg')
+        num_epochs = 25
+    elif model_name == 'Xception':
+        model = Xception(include_top=False, weights='imagenet', input_shape = (166, 166, 3), pooling = 'avg')
         num_epochs = 25
     else:
         print('Model name unknown, please choose ResNet50, Xception or InceptionResNetV2.')
         sys.exit()
 
-    x = Dense(3, activation='softmax')(model.output)
-    model = Model(inputs = model.input, outputs=x)
-    model.compile(loss='categorical_crossentropy',optimizer='sgd',metrics=['accuracy'])
-    #mc = ModelCheckpoint('weights{epoch:08d}.h5',save_weights_only=True, period=18)
-    
-    #Sets weights to compensate unbalanced data
-    y_ints = [y.argmax() for y in y_train]
-    class_weights = class_weight.compute_class_weight('balanced',np.unique(y_ints),y_ints)
-    class_weight_dict = dict(enumerate(class_weights.tolist()))
-    
-    #One hot encoding test set
-    y_test = np_utils.to_categorical(np.array(y_test)-1, num_classes = len(unique_labels(y_test)))
-    
-    #Train model
-    history = model.fit(x_train,y_train, validation_data = (x_test, y_test), epochs=num_epochs, batch_size=batch, shuffle = True, class_weight = class_weight_dict)
+    #Option for clustering-loss
+    if loss == 'clustering':
+        c1_m, c2_m, c3_m = get_centers(x_train, y_train, model)
+        y_train_reg = []
+        for y in y_train:
+             if y == 1:
+                 y_train_reg.append(c1_m)
+             elif y == 2:
+                 y_train_reg.append(c2_m)
+             elif y == 3:
+                 y_train_reg.append(c3_m)
+        y_test_reg = []
+        for y in y_test:
+            if y == 1:
+                y_test_reg.append(c1_m)
+            elif y == 2:
+                y_test_reg.append(c2_m)
+            elif y == 3:
+                 y_test_reg.append(c3_m)
+        model.compile(loss='mean_absolute_error', optimizer='sgd', metrics=['accuracy'])
+        model_name = model_name + '_clusteringloss'
+        y_train = np.asarray(y_train_reg)
+        y_test = np.asarray(y_test_reg)
+        #Train model
+        history = model.fit(x_train,y_train, validation_data = (x_test, y_test), epochs=num_epochs, batch_size=batch, shuffle = True)
+    else:
+        x = Dense(3, activation='softmax')(model.output)
+        model = Model(inputs = model.input, outputs=x)
+        model.compile(loss='categorical_crossentropy',optimizer='sgd',metrics=['accuracy'])
+        #Convert class vectors to binary class matrices
+        n_classes = len(unique_labels(y_train))
+        y_train = np_utils.to_categorical(np.array(y_train)-1, num_classes = n_classes)
+        y_test = np_utils.to_categorical(np.array(y_test)-1, num_classes = len(unique_labels(y_test)))
+        #Sets weights to compensate unbalanced data
+        y_ints = [y.argmax() for y in y_train]
+        class_weights = class_weight.compute_class_weight('balanced',np.unique(y_ints),y_ints)
+        class_weight_dict = dict(enumerate(class_weights.tolist()))
+        #Train model
+        history = model.fit(x_train,y_train, validation_data = (x_test, y_test), epochs=num_epochs, batch_size=batch, shuffle = True, class_weight = class_weight_dict)
     
     #Save history
-    with open('trainHistoryDict.p', 'wb') as file_pi:
+    with open('models/'+model_name+'.p', 'wb') as file_pi:
         pickle.dump(history.history, file_pi)
     
     #Save model
     model_json = model.to_json()
-    with open("InceptionResNetV2.json", "w") as json_file:
+    with open('models/'+model_name+'.json', "w") as json_file:
         json_file.write(model_json)
-    model.save_weights("InceptionResNetV2.h5")
-    pdb.set_trace()
-
-    return model
+    model.save_weights('models/'+model_name+'.h5')
 
 
 
-def load_model(model_name):
+def load_model(model_name, loss):
     
     #Load model structure
-    json_file = open('models/'+ model_name + '.json', 'r')
+    if loss == 'clustering':
+        model_name = model_name + '_clusteringloss'
+    json_file = open('models/'+model_name + '.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     loaded_model = model_from_json(loaded_model_json)
     
-    #model = VGG16(include_top=False, weights='imagenet', input_shape = (166, 166, 3))
-    #x = Flatten(input_shape=model.output.shape)(model.output)
-    #x = Dense(4096, activation='relu')(x)
-    #x = Dense(4096, activation='relu')(x)
-    #x = Dense(3, activation='relu')(x)
-    #loaded_model = Model(inputs = model.input, outputs=x)
-
-    #for layer in loaded_model.layers:
-    #    layer.trainable = False  # freeze layer
-    
     #Load weights into new model
-    loaded_model.load_weights('models/'+ model_name + '.h5')
+    loaded_model.load_weights('models/'+model_name + '.h5')
 
     return loaded_model
 
 
-def feature_extractor(x_test, y_test, model):
+def feature_extractor(x_test, y_test, model, loss):
     
     #Remove classification layer from model
-    layers = [layer.name for layer in model.layers]
-    model = Model(inputs = model.input, outputs=model.get_layer(layers[-2]).output)
+    if loss == 'clustering':
+        model = Model(inputs = model.input, outputs=model.layers[-1].output)
+    else:
+        model = Model(inputs = model.input, outputs=model.layers[-2].output)
     
     #Obtain features
     data_features = []
@@ -198,22 +217,6 @@ def clustering(features, labels, clusters, method):
     else:
         print('Please choose a valid clustering technique: Kmeans or AC')
         sys.exit()
-    
-    #Evaluate cluster accuracy hungarian algorithm
-    acc, corresp_labels = cluster_acc(np.array(labels), clustering.labels_)
-    print('The accuracy for the clustering is: ' + str(acc*100))
-
-    #final_labels = []
-    #for i in clustering.labels_:
-    #    final_labels.append(corresp_labels[i])
-    #pdb.set_trace()
-    #corresp_labels = corresp_labels.tolist()
-    #corresp_labels.sort()
-    
-    #conf_mat = confusion_matrix2(final_labels, labels)
-    # Plot normalized confusion matrix
-#plot_confusion_matrix(labels, final_labels, classes=np.asarray(corresp_labels), normalize=True, title='Normalized confusion matrix')
-#plt.show()
 
     clustering_labels = clustering.labels_.tolist()
 
@@ -227,38 +230,7 @@ def clustering(features, labels, clusters, method):
 
 
 
-def cluster_acc(y_true, y_pred):
-    '''
-        Uses the hungarian algorithm to find the best permutation mapping and then calculates the accuracy wrt
-        Implementation inpired from https://github.com/piiswrong/dec, since scikit does not implement this metric
-        this mapping and true labels
-        :param y_true: True cluster labels
-        :param y_pred: Predicted cluster labels
-        :return: accuracy score for the clustering
-        '''
-    D = int(max(y_pred.max(), y_true.max()) + 1)
-    w = np.zeros((D, D), dtype=np.int32)
-    for i in range(y_pred.size):
-        idx1 = int(y_pred[i])
-        idx2 = int(y_true[i])
-        w[idx1, idx2] += 1
-    ind = linear_sum_assignment(w.max() - w)
-    val = []
-    for i in range(len(ind[0])):
-        val.append(w[ind[0][i],ind[1][i]])
-    return sum(val) * 1.0 / y_pred.size, ind[1]
-
-
-
 def visualize_data(Z, labels, num_clusters):
-    '''
-        TSNE visualization of the points in latent space Z
-        :param Z: Numpy array containing points in latent space in which clustering was performed
-        :param labels: True labels - used for coloring points
-        :param num_clusters: Total number of clusters
-        :param title: filename where the plot should be saved
-        :return: None - (side effect) saves clustering visualization plot in specified location
-        '''
     tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
     Z_tsne = tsne.fit_transform(Z)
     fig = plt.figure()
@@ -266,90 +238,51 @@ def visualize_data(Z, labels, num_clusters):
     plt.colorbar(ticks=range(num_clusters))
     plt.show()
 
-    return
+
+
+def print_vectors_feats(feats, labels):
+    zipped = zip(feats, labels)
+    n_feats = len(feats[0])
+    x = np.linspace(0,n_feats, num=n_feats)
+    add_1 = np.zeros(n_feats)
+    add_2 = np.zeros(n_feats)
+    add_3 = np.zeros(n_feats)
+    c1 = 0
+    c2 = 0
+    c3 = 0
+    for f,l in zipped:
+        if l == 1:
+            add_1 += f
+            c1 += 1
+        elif l == 2:
+            add_2 += f
+            c2 += 1
+        elif l == 3:
+            add_3 += f
+            c3 += 1
+    add_1 = add_1 / c1
+    add_2 = add_2 / c2
+    add_3 = add_3 / c3
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 3, 1)
+    ax.plot(x, add_1, color='red')
+    ax.set_title('Features for class 1')
+    ax1 = fig.add_subplot(1, 3, 2)
+    ax1.plot(x, add_2, color='green')
+    ax1.set_title('Features for class 2')
+    ax2 = fig.add_subplot(1, 3, 3)
+    ax2.plot(x, add_3, color='blue')
+    ax2.set_title('Features for class 3')
+    plt.show()
 
 
 
-def confusion_matrix2(clusters, classes_gt):
-    new_gt = deepcopy(classes_gt)
-    l = list(set(classes_gt))
-    for i in range(len(classes_gt)):
-        for j in range(len(l)):
-            if classes_gt[i] == l[j]:
-                new_gt[i] = j
-    conf_mat = np.zeros([len(set(clusters)), len(set(new_gt))])
-    for i in range(len(clusters)):
-        conf_mat[clusters[i], new_gt[i]] += 1
+def plot_history(model_name, loss):
     
-    return conf_mat
-
-
-def activations(test_image, label, model):
-    #test_image = cv2.resize(test_image, (224, 224))
-    test_image = np.expand_dims(test_image, axis=0)
-    pdb.set_trace()
-    #layer_outputs = [layer.output for layer in model.layers[:5]]
-    num = 0
-    layer_outputs = [layer.output for layer in model.layers[8:9] if not layer.name.startswith('input')]
-    activation_model = Model(inputs=model.input, outputs=layer_outputs)
-    activations = activation_model.predict(test_image)
-    pdb.set_trace()
-    
-    for l in range(0, len(activations)):
-        fig=plt.figure()
-        columns = 8
-        rows = 4
-        for i in range(1, columns*rows +1):
-            activation = activations[l]
-            fig.add_subplot(rows, columns, i)
-            plt.imshow(activation[0, :, :, i-1], cmap='viridis')
-        fig.suptitle('Plotting layer: '+str(l+num))
-        plt.show()
-        pdb.set_trace()
-
-
-def testing_classification(model, x_test, y_test):
-    correct = 0
-    for i, image in enumerate(x_test):
-        #image = cv2.resize(image, (224, 224))
-        image = np.expand_dims(image, axis=0)
-        label = model.predict(image)
-        if label.tolist()[0].index(max(label[0]))+1 == y_test[i]:
-            correct+=1
-        
-            #print(str(correct) + ' correct out of: ' + str(len(y_test)))
-        print('True label: ' + str(y_test[i]), 'Predicted label: '+ str(label.tolist()[0].index(max(label[0]))+1))
-    acc = correct/len(x_test)*100
-    return acc
-
-
-
-def feat_repr(feats, labels):
-    class_1 = []
-    class_2 = []
-    class_3 = []
-    pdb.set_trace()
-    for i, f in enumerate(feats):
-        if labels[i] == 1:
-            class_1.append(f[941])
-        elif labels[i] == 2:
-            class_2.append(f[941])
-        else:
-            class_3.append(f[941])
-    threshold_1 = np.mean(class_1)
-    threshold_3 = np.mean(class_3)
-    predicted = []
-    true = []
-    for i, f in enumerate(feats):
-        if f[941] > threshold_1 and f[941] < threshold_3:
-            predicted.append(i)
-            true.append(labels[i])
-    pdb.set_trace()
-
-
-def plot_history(model_name):
+    if loss == 'clustering':
+        model_name = model_name + '_clusteringloss'
     history = pickle.load(open('models/' + model_name +'.p',"rb"))
-    
+    pdb.set_trace()
     #Plot accuracy
     x1 = np.linspace(1,len(history['acc']),len(history['acc']))
     y1 = history['acc']
@@ -376,10 +309,43 @@ def plot_history(model_name):
 
 
 
+def get_centers(x_train, y_train, model):
+    if os.path.exists('center_1.csv'):
+        c1_m = np.genfromtxt('center_1.csv', dtype=None)
+        c2_m = np.genfromtxt('center_2.csv', dtype=None)
+        c3_m = np.genfromtxt('center_3.csv', dtype=None)
+    else:
+        train = zip(x_train, y_train)
+        c1 = []
+        c2 = []
+        c3 = []
+        for idx, x in enumerate(train):
+            print('Taking image: '+str(idx))
+            img = preprocess_input(np.expand_dims(x[0], axis=0))
+            pred = model.predict(img)
+            if x[1] == 1:
+                c1.append(pred.flatten())
+            elif x[1] == 2:
+                c2.append(pred.flatten())
+            elif x[1] == 3:
+                c3.append(pred.flatten())
+            else:
+                print('Unknown class')
+        c1_m = np.mean(np.asarray(c1), axis=0)
+        np.savetxt('center_1.csv', c1_m, delimiter=',', fmt='%f')
+        c2_m = np.mean(np.asarray(c2), axis=0)
+        np.savetxt('center_2.csv', c2_m, delimiter=',', fmt='%f')
+        c3_m = np.mean(np.asarray(c3), axis=0)
+        np.savetxt('center_3.csv', c3_m, delimiter=',', fmt='%f')
+
+    return c1_m, c2_m, c3_m
+
+
+
 if __name__ == "__main__":
     train_path = 'dataset/train/'
     test_path = 'dataset/test/'
-    batch = 56
+    batch = 16
     num_clusters = 3
     args = parse_args()
     
@@ -387,22 +353,22 @@ if __name__ == "__main__":
     x_train, y_train, x_test, y_test = data_preparation(train_path, test_path)
     
     print('Training begins...')
-    model = train_model(x_train, y_train, batch, x_test, y_test, args.model)
-    #plot_history(args.model)
+    train_model(x_train, y_train, batch, x_test, y_test, args.model, args.loss)
+    #plot_history(args.model, args.loss)
     
     print('Loading model...')
-    model = load_model(args.model)
-
-    #print('Show activations')
-    #activations(x_test[0], y_test[0], model)
+    model = load_model(args.model, args.loss)
     
     print('Data features are now begining to extract...')
-    data_features, labels = feature_extractor(x_test,y_test, model)
-    
+    data_features, labels = feature_extractor(x_test,y_test, model, args.loss)
+
     #Visualizing the data
     visualize_data(data_features, labels, num_clusters)
     
-    print('Clustering begining...')
+    print('Clustering the features...')
     clustering(data_features, labels, num_clusters, args.clustering_method)
+
+    print('Show features for the classes...')
+    print_vectors_feats(data_features, labels)
 
 
